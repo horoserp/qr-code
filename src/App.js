973cdee9ba0fdd3b71ed4ef93b844deccd8d8564
Robert Horosewski <horoserp@yahoo.com>
2024-12-18 15:19:06 -0500

    Update: Finish desktop styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <main>
+      <div className="container">
+        <img src="/qr-code/images/image-qr-code.png" alt="QR Code" />
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
