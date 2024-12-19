53822ada66db7a38de5869766d24de84280b4ef8
Robert Horosewski <horoserp@yahoo.com>
2024-12-19 12:10:03 -0500

    Fix: Change container display to flex and add gap to replace margin-bottom in img.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
     <main>
       <div className="container">
         <img src="/qr-code/images/image-qr-code.png" alt="QR Code" />
-        <h1>Improve your front-end skills by building projects</h1>
-        <p>
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
+        <div className="content">
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </div>
     </main>
   );
