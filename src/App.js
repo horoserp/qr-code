1abc552a037662e2b89e3a904bc417ec9f976543
Robert Horosewski <horoserp@yahoo.com>
2025-02-02 22:49:07 -0500

    FIX: Make react app run correctly.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <main>
-      <div className="container">
-        <img src="/qr-code/images/image-qr-code.png" alt="QR Code" />
-        <div className="content">
-          <h1>Improve your front-end skills by building projects</h1>
-          <p>
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
-        </div>
-      </div>
-    </main>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
